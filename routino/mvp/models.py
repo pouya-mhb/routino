29d334cd6695a0e16c6610ee81c8174e18f34f49
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2024-02-22 21:47:40 +0330

    Update models.py

--- routino/mvp/models.py
+++ routino/mvp/models.py
@@ -20,6 +20,51 @@ class Profile (models.Model):
     gender = models.CharField(choices=gender_choices, max_length=6)
 
 
+class Activity (models.Model):
+    STATUS_CHOICES = (
+        ('new', 'New'),
+        ('committed', 'Committed'),
+        ('doing', 'Doing'),
+        ('done', 'Done'),
+    )
+
+    profile = models.ForeignKey(Profile,
+                                on_delete=models.CASCADE)
+
+    status = models.CharField(
+        max_length=100, choices=STATUS_CHOICES, default='new')
+
+    title = models.CharField(max_length=250)
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(auto_now_add=True)
+    desciption = models.TextField()
+
+
+class Routine (models.Model):
+    # def get_profile_id ():
+    #     Profile.objects.get(id)
+
+    profile = models.ForeignKey(Profile,
+                                on_delete=models.CASCADE,
+                                related_name='routine_profile',
+                                verbose_name="Routine Profile",
+                                default=1)
+
+    activity = models.ForeignKey(
+        Activity,
+        on_delete=models.CASCADE,
+        related_name='routine_activity',
+        verbose_name="Routine Activity",
+        default=1)
+
+    title = models.CharField(max_length=250)
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(auto_now_add=True)
+    desciption = models.TextField()
+
+
 class RoutineGoal (models.Model):
 
     STATUS_CHOICES = (
@@ -43,39 +88,10 @@ class RoutineGoal (models.Model):
         max_length=100, choices=GOAL_DURATION_CHOICES, default='0')
 
     profile = models.ForeignKey(Profile,
-                                on_delete=models.CASCADE)
+                                on_delete=models.CASCADE, default=1)
 
-    title = models.CharField(max_length=250)
-    start_date = models.DateTimeField(default=timezone.now)
-    end_date = models.DateTimeField(default=timezone.now)
-    created_date = models.DateTimeField(auto_now_add=True)
-    desciption = models.TextField()
-
-
-class Activity (models.Model):
-    STATUS_CHOICES = (
-        ('new', 'New'),
-        ('committed', 'Committed'),
-        ('doing', 'Doing'),
-        ('done', 'Done'),
-    )
-
-    profile = models.ForeignKey(Profile,
-                                on_delete=models.CASCADE)
-
-    status = models.CharField(
-        max_length=100, choices=STATUS_CHOICES, default='new')
-
-    title = models.CharField(max_length=250)
-    start_date = models.DateTimeField(default=timezone.now)
-    end_date = models.DateTimeField(default=timezone.now)
-    created_date = models.DateTimeField(auto_now_add=True)
-    desciption = models.TextField()
-
-
-class Routine (models.Model):
-    activity = models.ForeignKey(Activity,
-                                 on_delete=models.CASCADE)
+    routine = models.ForeignKey(Routine,
+                                on_delete=models.CASCADE, default=1)
 
     title = models.CharField(max_length=250)
     start_date = models.DateTimeField(default=timezone.now)
