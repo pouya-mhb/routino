6e51a98ac7068759f2ee1a5e888d47dffb54e671
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2024-02-22 21:00:01 +0330

    add admin models

--- routino/mvp/admin.py
+++ routino/mvp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from mvp import models
 # Register your models here.
+
+admin.site.register(models.Profile)
+admin.site.register(models.RoutineGoal)
+admin.site.register(models.Routine)
+admin.site.register(models.Activity)
