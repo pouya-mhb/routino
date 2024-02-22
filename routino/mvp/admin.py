ede4675a575d8dd226b672ea52e5996d2f9dd5e3
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2024-02-22 21:28:41 +0330

    Update admin.py

--- routino/mvp/admin.py
+++ routino/mvp/admin.py
@@ -2,7 +2,40 @@ from django.contrib import admin
 from mvp import models
 # Register your models here.
 
-admin.site.register(models.Profile)
-admin.site.register(models.RoutineGoal)
-admin.site.register(models.Routine)
+# admin.site.register(models.Profile)
+# admin.site.register(models.RoutineGoal)
+# admin.site.register(models.Routine)
 admin.site.register(models.Activity)
+
+
+@admin.register(models.Profile)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'user_profile', 'firstName', 'lastName', 'userName', 'age', 'gender'
+    ]
+
+
+@admin.register(models.RoutineGoal)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'status',
+        'goal_duration',
+        'profile',
+        'title',
+        'start_date',
+        'end_date',
+        'created_date',
+        'desciption'
+    ]
+
+
+@admin.register(models.Routine)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'activity',
+        'title',
+        'start_date',
+        'end_date',
+        'created_date',
+        'desciption'
+    ]
