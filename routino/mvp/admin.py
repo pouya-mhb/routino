4d8b5121bc19fdd167da4a820a1fa8f3a22df9a7
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2024-02-23 15:25:34 +0330

    improve admin panel

--- routino/mvp/admin.py
+++ routino/mvp/admin.py
@@ -5,7 +5,24 @@ from mvp import models
 # admin.site.register(models.Profile)
 # admin.site.register(models.RoutineGoal)
 # admin.site.register(models.Routine)
-admin.site.register(models.Activity)
+# admin.site.register(models.Activity)
+# admin.site.register(models.ActivityType)
+
+
+@admin.register(models.ActivityType)
+class ActivityTypeAdmin (admin.ModelAdmin):
+    list_display = ['subject', 'title', 'desciption', 'created_date']
+
+
+@admin.register(models.Activity)
+class ActivityAdmin (admin.ModelAdmin):
+    list_display = [
+        'title',
+        'profile',
+        'status',
+        'frequency'
+
+    ]
 
 
 @admin.register(models.Profile)
