87b242b42c214ec5f84e04f3b72e6e9c0339997c
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2024-02-28 17:13:43 +0330

    Update admin.py

--- routino/mvp/admin.py
+++ routino/mvp/admin.py
@@ -24,6 +24,7 @@ class PostAdmin(admin.ModelAdmin):
 class PostAdmin(admin.ModelAdmin):
     list_display = [
         'title',
+        'id',
         'status',
         'frequency',
         'profile',
