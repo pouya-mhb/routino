fd8a4b9120c201261106e76cf7aa001e5b662d4f
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2024-02-28 20:06:30 +0330

    add duplicate action to admin + Update

--- routino/mvp/admin.py
+++ routino/mvp/admin.py
@@ -2,41 +2,105 @@ from django.contrib import admin
 from mvp import models
 
 
-@admin.register(models.Activity)
-class ActivityAdmin (admin.ModelAdmin):
+@admin.action(description='Duplicate selected record')
+def duplicate_row(modeladmin, request, queryset):
+    for object in queryset:
+        object.id = None
+        object.save()
+
+
+admin.site.add_action(duplicate_row)
+
+
+@admin.register(models.Category)
+class Activity_Category_Admin (admin.ModelAdmin):
     list_display = [
         'title',
-        'profile',
-        'status',
-        'frequency'
+        # 'category',
+        'description',
+        'score',
+        'created_date'
 
     ]
 
 
+@admin.register(models.Status)
+class Activity_Status_Admin (admin.ModelAdmin):
+    list_display = [
+        'title',
+        'score',
+        'id',
+        'description',
+        'created_date'
+
+    ]
+
+
+@admin.register(models.Frequency)
+class Activity_Frequency_Admin (admin.ModelAdmin):
+    list_display = [
+        'title',
+        'score',
+        'id',
+        'description',
+        'created_date'
+    ]
+
+
+@admin.register(models.SubCategory)
+class Activity_SubCategory_Admin (admin.ModelAdmin):
+    list_display = [
+        'title',
+        'score',
+        'id',
+        'description',
+        'created_date'
+    ]
+
+
+@admin.register(models.Activity)
+class Activity_Admin (admin.ModelAdmin):
+    list_display = [
+        'title',
+        'id',
+        'category',
+        'type',
+        'status',
+        'frequency',
+        'description',
+        'profile'
+    ]
+
+
 @admin.register(models.Profile)
-class PostAdmin(admin.ModelAdmin):
+class Profile_Admin(admin.ModelAdmin):
     list_display = [
         'user_profile', 'firstName', 'lastName', 'userName', 'age', 'gender'
     ]
 
 
 @admin.register(models.Goal)
-class PostAdmin(admin.ModelAdmin):
+class Goal_Admin(admin.ModelAdmin):
     list_display = [
         'title',
+        'profile',
         'id',
         'status',
-        'frequency',
-        'profile',
+        'routine',
+        'activity',
         'start_date',
         'end_date',
         'created_date',
         'desciption'
     ]
 
+    ordering = ['id']
+
+    actions = ['duplicate_row']
+
 
 @admin.register(models.Routine)
-class PostAdmin(admin.ModelAdmin):
+class Routine_Admin(admin.ModelAdmin):
     list_display = [
         'profile',
         'activity',
